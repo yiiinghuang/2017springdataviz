65774fa7db4a75b1e0da73482a0112ce0a12d546
yiiinghuang <mac@lengtianxiaokeai.local>
2017-03-30 22:22:19 -0400

    Coding Exercise - 03

--- js/sketch.js
+++ js/sketch.js
@@ -1,70 +1,45 @@
 // ***** Global variables ***** //
-var refugeeTable;
-var maxTotal = 0;
-var maxLabel = 0;
-var maxLength = 550;
-var topRefugeeCountries = new p5.Table;
-var headers = ['Country','Refugees','Asylum-seekers','Returned refugees','IDPs','Returned IDPs','Stateless','Others of concern','Total']
+
+var apiKey = '45c66834e16c6a60b0cc926ba1f82cf6';
+var baseURL = 'http://api.openweathermap.org/data/2.5/weather?q=';
+var city;
+var units = 'metric';
+var weatherData;
+var temperature = 0;
+var humidity = 0;
+var button;
 
 
-// ***** Preload function ***** //
-function preload(){
-  refugeeTable = loadTable('../data/RefugeesUNHCR1.csv', 'csv', 'header');
-  console.log('Done loading table...');
-}
 
 // ***** Setup function ***** //
+
 function setup(){
-  createCanvas(800, 3000);
-  textSize(12);
-  console.log('Setup complete...');
-  print(refugeeTable.getRowCount() + ' rows loaded...');
-  print(refugeeTable.getColumnCount() + ' columns loaded...');
-  for (var i = 0; i < refugeeTable.getRowCount(); i++) {
-    maxTotal = max(refugeeTable.getNum(i, 'Total'), maxTotal);
-    maxLabel = max(refugeeTable.getString(i, 'Country').length, maxLabel);
-  }
-  print('Maximum total is ' + maxTotal);
-  print('Maximum label length is ' + maxLabel);
-  creatNewTable();
+  createCanvas(1000,1000);
+  button = select('#submit');
+  city = select('#city');
+  button.mousePressed(queryAPI);
 }
 
-function creatNewTable(){
-  //write a loop to add column title
-  for (var i = 0; i < headers. length; i++){
-    topRefugeeCountries.addColumn(headers[i]);
-  }
-    //topRefugeeCountries.addColumn('Country');
-    //topRefugeeCountries.addColumn('Total');
-for(var i = 0; i < refugeeTable.getRowCount(); i++){
-   var totalRefugees = refugeeTable.getNum(i,'Total');
-   if (totalRefugees >= 100000){
-      var newRow = topRefugeeCountries.addRow();
-      for (var j = 0; j < headers.length; j++){
-      newRow.setString(headers[j],refugeeTable.getString(i, headers[j]));
-      }
-    }
-  }
-  print('New top refugee table created...');
+function queryAPI(){
+    var query = baseURL + city.value()+ '&apiKey=' + apiKey + '&units=' + units;
+    loadJSON(query,getWeatherData);
+}
 
-}
-function drawCountries(category){
-    fill(0);
-    noStroke();
-    textAlign(LEFT, TOP);
-    for (var i = 0; i < topRefugeeCountries.getRowCount(); i++) {
-        var total = topRefugeeCountries.getNum(i, category);
-        var length = map(total, 0, maxTotal, 0, maxLength);
-        rect(maxLabel * 5, 2 + 14*i, length, 12);
-        text(nfc(total, 0), maxLabel * 5 + length + 5, 14*i);
-    }
-    textAlign(RIGHT, TOP);
-    for (var i = 0; i < topRefugeeCountries.getRowCount(); i++) {
-        text(topRefugeeCountries.getString(i, 'Country'), maxLabel * 5 - 5, 14*i);
-    }
-}
+ function getWeatherData(apiData){
+   weatherData = apiData;
+   temperture = weatherData.main.temp;
+   humidity = weatherData.main.humidity;
+   console.log(weatherData);
+ }
+
+// ***** Draw function ***** //
+
 
 function draw(){
-    background(255);
-    drawCountries('Asylum-seekers');
-}
+  background(255);
+  fill(0);
+  noStroke();
+  if (weatherData){
+    ellipse(300, 300, temperture * 10, humidity * 5);
+    }
+  }
